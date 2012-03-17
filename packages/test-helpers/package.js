9bb42438968a5a6c9bad1bae2a3cb7b6f43fe460
Geoff Schmidt <geoff@geoffschmidt.com>
2012-03-17 01:54:21 -0700

    new query diffing algorithm with fuzz tests

--- packages/test-helpers/package.js
+++ packages/test-helpers/package.js
@@ -9,6 +9,7 @@ Package.on_use(function (api, where) {
   api.add_files('try_all_permutations.js', where);
   api.add_files('async_multi.js', where);
   api.add_files('test_events.js', where);
+  api.add_files('seeded_random.js', where);
 });
 
 Package.on_test(function (api) {
