0c19448e76e97076a4ea80c2c11152948dfe5ad3
Nick Martin <nim@meteor.com>
2012-03-23 14:28:56 -0700

    Add API for diff operation. Not yet implemented, but agreed and specified.

---
+++ packages/minimongo/diff.js
@@ -0,0 +1,6 @@
+// old_result: array of documents.
+// new_result: array of documents.
+// observer: object with 'added', 'changed', 'moved', 'removed' functions
+LocalCollection._diffQuery = function (old_result, new_result, observer) {
+  // XXX implement
+};
